3a9a991e40176c0b3822cd2087db94ad67938d93
Георгий Нор <Nor.Junior@ya.ru>
2022-08-19 21:01:15 +0300

    My first commit in this project

---
+++ yatube/posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    # Страница с группами
+    path('group/<slug:slug>/', views.group_posts, name = 'group_posts'),
+    # Главная страница
+    path('', views.index, name = 'main'),
+] 
