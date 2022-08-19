ee078be09f94cdeb9dc183a3d5183678bc7e4bf0
Георгий Нор <Nor.Junior@ya.ru>
2022-08-19 21:18:07 +0300

    my second commit

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,17 +1,17 @@
-from enum import unique
 from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique = True)
-    
+    slug = models.SlugField(unique=True)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
@@ -25,6 +25,6 @@ class Post(models.Model):
     group = models.ForeignKey(
         Group,
         on_delete=models.CASCADE,
-        blank=True, 
+        blank=True,
         null=True
     )
